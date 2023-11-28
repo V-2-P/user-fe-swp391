38d75cf73a973eabf3c227520c259a9000fb93ec
Trần Thị Hồng Phúc <142323640+meo7mau@users.noreply.github.com>
2023-11-28 13:01:42 +0700

    Update bookingDetail.tsx

--- src/application/pages/Booking/bookingDetail.tsx
+++ src/application/pages/Booking/bookingDetail.tsx
@@ -382,7 +382,7 @@ const BookingDetailPage: React.FC = () => {
   const handleNotTakeBird = async () => {
     setBtnNotTakeBirdLoading(true)
     try {
-      const response = await axiosClient.put(`/bookingdetail/${id}/status?status=Not_Receiving_Confirm`)
+      const response = await axiosClient.put(`/bookingdetail/${booking.bookingDetail.id}/status?status=Not_Receiving_Confirm`)
       if (response) {
         dispatch(reFetchData())
         setBtnNotTakeBirdLoading(false)
